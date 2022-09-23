dd125db54721110fa2898509ff218240260f90ca
salut <killian.portier@yahoo.fr>
2022-09-23 10:55:09 +0200

    big fix menu burger

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let toggle = document.querySelector('.toggle');
 let body = document.querySelector('body');
+// let rocket = document.querySelector('go-home')
 
 toggle.addEventListener('click', function() {
     body.classList.toggle('open');
 })
+
+// rocket.addEventListener('click', function() {
+
+// })
