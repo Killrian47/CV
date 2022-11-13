ee0bd4a70270a374483141725dd8c8a3a13f2b6c
salut <killian.portier@yahoo.fr>
2022-11-13 21:04:05 +0100

    test to fix cv css

--- CV2.php
+++ CV2.php
@@ -1,6 +1,48 @@
-<?php
-    include "header.php";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Killian's website</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+<a href="#" class="go-to-top">
+    <i class="fas fa-arrow-up"></i>
+</a>
+
+<header>
+    <nav>
+        <div class="logo">
+            <a href="/"><i class="fas fa-rocket go-home"></i></a>
+        </div>
+
+        <div class="toggle2">
+            <i class="fas fa-toggle-off light"></i>
+            <i class="fas fa-toggle-on dark"></i>
+        </div>
+
+        <div class="toggle3">
+            <i class="fas fa-language lang"></i>
+        </div>
+
+        <div class="toggle">
+            <i class="fas fa-bars ouvrir"></i>
+            <i class="fas fa-times fermer"></i>
+        </div>
+
+        <ul class="menu">
+            <li class="fr"><a href="index.php">Accueil</a></li>
+            <li class="fr"><a href="CV2.php">Mon CV</a></li>
+            <li class="en"><a href="index.php">Home</a></li>
+            <li class="en"><a href="CV2.php">My CV</a></li>
+            <!-- <li><a href="contact.php">Contact me</a></li> -->
+        </ul>
+    </nav>
+</header>
 <section class="big-container">
         <div class="left-container">
             <section class="who">
